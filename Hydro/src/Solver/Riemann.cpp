31d0c6b8302dfe98591f9a455cfea42ed59e315d
Alexey Rybakov <rybakov@ians.aero>
2016-01-29 11:45:33 +0300

    Hydro : Riemann problem skeleton

---
+++ Hydro/src/Solver/Riemann.cpp
@@ -0,0 +1,28 @@
+/**
+ * \file
+ * \brief Riemann problem solver realization.
+ *
+ * \author Alexey Rybakov
+ */
+
+#include "Riemann.h"
+
+namespace Hydro { namespace Solver {
+
+/**
+ * \brief Solve Riemann problem simple by getting average values.
+ */
+void Riemann::Avg(const Fluid_Dyn_Pars *left_p,
+                  const Fluid_Dyn_Pars *right_p,
+                  Fluid_Dyn_Pars *center_p)
+{
+    center_p->V.X = 0.5 * (left_p->V.X + right_p->V.X);
+    center_p->V.X = 0.5 * (left_p->V.Y + right_p->V.Y);
+    center_p->V.X = 0.5 * (left_p->V.Z + right_p->V.Z);
+    center_p->T = 0.5 * (left_p->T + right_p->T);
+    center_p->Ro = 0.5 * (left_p->Ro + right_p->Ro);
+    center_p->P = 0.5 * (left_p->P + right_p->P);
+}
+
+} }
+
---
+++ Hydro/src/Solver/Riemann.h
@@ -0,0 +1,37 @@
+/**
+ * \file
+ * \brief Riemann problem solver.
+ *
+ * \author Alexey Rybakov
+ */
+
+#ifndef HYDRO_SOLVER_RIEMANN_H
+#define HYDRO_SOLVER_RIEMANN_H
+
+#include "Grid/Fluid_Dyn_Pars.h"
+
+using namespace Hydro::Grid;
+
+namespace Hydro { namespace Solver {
+
+/**
+ * \brief Riemann problem solver.
+ */
+class Riemann
+{
+
+public:
+
+    // Simple calculate by average values.
+    void Avg(const Fluid_Dyn_Pars *left_p,
+             const Fluid_Dyn_Pars *right_p,
+             Fluid_Dyn_Pars *center_p);
+
+private:
+
+};
+
+} }
+
+#endif
+
